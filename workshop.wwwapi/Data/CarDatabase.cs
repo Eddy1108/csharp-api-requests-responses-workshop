7b23ac8dad5d8f00ed5d767bece1928ae09311ce
Nigel <nigel.sibbert@gmail.com>
2024-01-24 08:03:53 +0000

    added endpoint, repository

---
+++ workshop.wwwapi/Data/CarDatabase.cs
@@ -0,0 +1,20 @@
+﻿using workshop.wwwapi.Models;
+
+namespace workshop.wwwapi.Data
+{
+    public class CarDatabase
+    {
+        private static List<Car> _cars = new List<Car>()
+        {
+            new Car(){ Id=1, Make="Mini", Model="Clubman"},
+            new Car(){ Id=2, Make="Mini", Model="One"},
+            new Car(){ Id=3, Make="Mini", Model="Countryman"}
+
+        };
+
+        public List<Car> GetCars()
+        {
+            return _cars;
+        }
+    }
+}
---
+++ workshop.wwwapi/Endpoints/CarEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using workshop.wwwapi.Repository;
+
+namespace workshop.wwwapi.Endpoints
+{
+    public static class CarEndpoint
+    {
+        public static void ConfigureCarEndpoint(this WebApplication app)
+        {
+            var carGroup = app.MapGroup("cars");
+
+            carGroup.MapGet("/", GetCars);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public static async Task<IResult> GetCars(IRepository repository)
+        {
+            return TypedResults.Ok(repository.GetCars());
+        }
+
+    }
+}
